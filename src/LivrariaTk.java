cc29850497295354bd0d54db945c0f0ba09d86a0
THASSO <thassokmorais@gmail.com>
2024-06-27 03:44:45 -0300

    Implementaçao dos metodos

--- src/LivrariaTk.java
+++ src/LivrariaTk.java
@@ -1,5 +1,8 @@
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
@@ -25,19 +28,50 @@ public class LivrariaTk {
             System.out.println("Não possui nenhum livro!");
         }
     }
-    public void exibirLivrosAutor( ){
+    public void exibirLivrosAutor(String nomeAutor){
+        boolean encontrouLivros = false;
+        System.out.println("Livros do autor " + nomeAutor + ":");
+        for (Livro livro : livros) {
+            if (livro.getAutor().equalsIgnoreCase(nomeAutor)) {
+                System.out.println(livro);
+                encontrouLivros = true;
+            }
+        }
+        if (!encontrouLivros) {
+            System.out.println("Não há livros do autor " + nomeAutor + " na livraria.");
+        }
+    }
+    public void exibirLivrosGenero(String livroGenero){
+        boolean encontrarGenero = false;
+        for (Livro livro : livros){
+            if (livro.getGenero().equalsIgnoreCase(livroGenero)){
+                System.out.println(livro);
+                encontrarGenero = true;
+            }
+        }
+        if (!encontrarGenero){
+            System.out.println("Não há livros do genero " + livroGenero + " na livraria.");
+        }
 
     }
-    public void exibirLivrosGenero(){
-
+    public BigDecimal calcularValorTotalLivros(){
+        BigDecimal valorTotal = BigDecimal.ZERO;
+        for (Livro livro : livros){
+            valorTotal = valorTotal.add(livro.getValorDeReserva());
+        }
+        return valorTotal;
     }
-    public void calcularValorTotalLivros(){
-
+    public Livro livroMaisCaro() {
+        if (livros.isEmpty()) {
+            return null;
+        }
+        return Collections.max(livros, Comparator.comparing(Livro::getValorDeReserva));
     }
-    public void livroMaisCaro(){
 
-    }
-    public void livroMaisBarato(){
-
+    public Livro livroMaisBarato(){
+        if (livros.isEmpty()){
+            return null;
+        }
+        return Collections.min(livros, Comparator.comparing(Livro::getValorDeReserva));
     }
 }
